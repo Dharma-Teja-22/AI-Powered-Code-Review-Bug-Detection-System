8f6bb364abff6b4fd41c131ac24e6d69f9cb8cb3
Dharma-Teja-22 <dharmateja0202@gmail.com>
2025-02-20 16:55:00 +0530

    Added public dist file

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,11 +1,14 @@
 import axios from "axios";
-const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+// const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+const SERVER_URL = window.location.origin;
+
 export default {
   post: {
     fetchCodeReviewResponse: async (lang, code) => {
       try
       {
-        const response = await axios.post(`${SERVER_URL}/review`, {lang, code});
+        // const response = await axios.post(`${SERVER_URL}/review`, {lang, code});
+        const response = await axios.post(`${SERVER_URL}/v1/api/review`, {lang, code});
         return response.data;
       }
       catch(err)
